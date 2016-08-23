0e53e0f43238e282c6c6958509dac881c98503cd
Alvaro Sanz <alvaro.sanz@aernnova.com>
2016-08-23 18:03:36 +0200

    setup.py modified

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ from distutils.core import setup
 setup(
     name='nastranpy',
     version='0.1',
+    description='A library to interact with nastran models',
+    url='https://bitbucket.org/alvarosanz/nastranpy',
+    author='Alvaro Sanz Oriz',
+    author_email='alvaro.sanz@aernnova.com',
     packages=['nastranpy',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    install_requires=['numpy'],
 )
