d2dcfd17ccbb917250cf17abd8ce1bf085c405a0
Alvaro Sanz <alvaro.sanz@aernnova.com>
2017-02-07 17:48:54 +0100

    Added card_help function

--- nastranpy/__init__.py
+++ nastranpy/__init__.py
@@ -1,7 +1,26 @@
 from nastranpy.bdf.model import Model
 from nastranpy.bdf.cards.card import Card
+from nastranpy.bdf.cards.card_interfaces import card_interfaces
 from nastranpy.bdf.read_bdf import cards_in_file
 from nastranpy.bdf.write_bdf import print_card
 from nastranpy.setup_logging import setup_logging
+
+
+def card_help(card_name):
+    """
+    Print help of a given card.
+
+    Parameters
+    ----------
+    card_name : str
+        Name of the card.
+
+    Examples
+    --------
+    >>> get_card_help('GRID')
+    """
+
+    help(card_interfaces[card_name])
+
 
 setup_logging()
